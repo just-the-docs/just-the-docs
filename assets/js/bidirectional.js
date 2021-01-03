40067873e33ebde61d156b086eed5e20d3797020
Your Name <you@example.com>
2021-01-03 01:39:33 -0800

    lowercase call in wrong spot

--- assets/js/bidirectional.js
+++ assets/js/bidirectional.js
@@ -7,8 +7,8 @@ fetch('/ops/references.json')
   .then(response => response.json())
   .then(data => {
      console.log(data)
-     const toks = (window.location + '').split("/").toLowerCase()
-     const my_slug = toks[toks.length-1]
+     const toks = (window.location + '').split("/")
+     const my_slug = toks[toks.length-1].toLowerCase()
      if (my_slug === 'ops') my_slug = 'index'
      const footer = document.querySelector('footer.page-footer')
      footer.innerHTML += "<h3>references</h3><ol><li>" + (data[my_slug] || []).join('</li><li>') + "</li></ol>"
