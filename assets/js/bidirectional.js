9c32a7ce2f1530951ab369e0cf040fcdbae1050a
Your Name <you@example.com>
2021-01-03 01:31:27 -0800

    load refs

--- assets/js/bidirectional.js
+++ assets/js/bidirectional.js
@@ -3,10 +3,15 @@
 
 console.log('setting up bidirectional links')
 
-const footer = document.querySelector('footer.page-footer')
-footer.innerHTML += `
-<h3>references</h3>
-
-`
+fetch('/ops/references.json')
+  .then(response => response.json())
+  .then(data => {
+     console.log(data)
+     const toks = (window.location + '').split("/").toLowerCase()
+     const my_slug = toks[toks.length-1]
+     if (my_slug === 'ops') my_slug = 'index'
+     const footer = document.querySelector('footer.page-footer')
+     footer.innerHTML += "<h3>references</h3><ol><li>" + (data[my_slug] || []).join('</li><li>') + "</li></ol>"
+});
 
 })();
