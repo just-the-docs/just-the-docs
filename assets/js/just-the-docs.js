bf87ee7086a043e90a564eff9b351ba47a298e71
Felix Krause <contact@flyx.org>
2023-02-15 22:48:01 +0100

    Fix style of listings for AsciiDoc (#1163)
    These changes accommodate for some difference in the HTML structure
    AsciiDoc produces for source code listings:
    
     * proper padding for source code listings
     * proper vertical margin after source code listings
     * proper placement of the copy button if enabled

--- assets/js/just-the-docs.js
+++ assets/js/just-the-docs.js
@@ -488,7 +488,7 @@ jtd.onReady(function(){
 
 jtd.onReady(function(){
 
-  var codeBlocks = document.querySelectorAll('div.highlighter-rouge, div.listingblock, figure.highlight');
+  var codeBlocks = document.querySelectorAll('div.highlighter-rouge, div.listingblock > div.content, figure.highlight');
 
   // note: the SVG svg-copied and svg-copy is only loaded as a Jekyll include if site.enable_copy_code_button is true; see _includes/icons/icons.html
   var svgCopied =  '<svg viewBox="0 0 24 24" class="copy-icon"><use xlink:href="#svg-copied"></use></svg>';
