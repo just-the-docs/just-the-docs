220ec1017970a85111b2da3daa508919e9bfbda2
Matthew Wang <matt@matthewwang.me>
2023-02-18 18:12:21 -0800

    add CI action

--- .percy.js
+++ .percy.js
@@ -2,6 +2,7 @@
 module.exports = {
   version: 2,
   static: {
-    include: ["docs/**/*.html", "404.html", "index.html"],
+    // include: ["docs/**/*.html", "404.html", "index.html"],
+    include: ["/docs/index-test/index.html"],
   },
 }
