434fc4490021f815877ec59b7d4511555f59b956
Mohammad Bayat <OkBayat@gmail.com>
2023-08-29 18:48:57 +0330

    add my_plugin.rb

---
+++ _plugins/my_plugin.rb
@@ -0,0 +1,7 @@
+# فایل: _plugins/custom_converter.rb
+
+Jekyll::Hooks.register :documents, :pre_render do |doc|
+  doc.content.gsub!(/\^([^\^]+)\^/) do
+    "<span class='test-class'>#{$1}</span>"
+  end
+end
