f6d4d766b83608e82c7d8e70764c31b8022b8edb
Mohammad Bayat <OkBayat@gmail.com>
2023-08-31 21:25:09 +0330

    Update my_plugin.rb

--- _plugins/my_plugin.rb
+++ _plugins/my_plugin.rb
@@ -16,17 +16,6 @@ class Jekyll::Converters::Markdown::MyCustomProcessor
     # { bage bold new | Example text } to <span class="bage bold new">Example text</span>
     html.gsub!(/\{\s*([^|]+?)\s*\\\s*(.+?)\s*\}/, '<span class="\1">\2</span>')
 
-    # جایگزینی فهرست مطالب با نسخه wrapped درون تگ‌های <details> و <summary>
-    toc_placeholder_regex = /\\TOC/
-    if html[toc_placeholder_regex]
-      html.gsub!(toc_placeholder_regex, '')
-      toc_html = '<details open markdown="block"><summary>فهرست مطالب</summary>'
-      toc_content = Kramdown::Document.new(content, toc_levels: [1, 2, 3, 4, 5, 6], generate_toc: true).toc_content
-      toc_html += toc_content
-      toc_html += '</details>'
-      html = toc_html + html
-    end
-
     html
   end
 end
