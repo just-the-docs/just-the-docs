603309007b35eab9ac8bf60dcc190919abd5749f
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2025-03-05 14:29:29 +0100

    scripts/verify_and_test.py         modified:   scripts/verify_python_build.py

--- scripts/verify_and_test.py
+++ scripts/verify_and_test.py
@@ -113,7 +113,7 @@ def main():
     tested_platforms_file_name = f"{ branch }_tested_platforms{ nightly_build }_{ arch }.csv"
 
     full_sha = get_full_sha(run_id)
-    if branch is 'main':
+    if branch == 'main':
         extensions = list_extensions()
     else:
         extensions=('arrow' 'autocomplete' 'aws' 'azure' 'delta' 'excel' 'fts' 'httpfs' 'iceberg' 'icu' 'inet' 'jemalloc' 'json' 'motherduck' 'mysql_scanner' 'parquet' 'postgres_scanner' 'shell' 'spatial' 'sqlite_scanner' 'sqlsmith' 'substrait' 'tpcds' 'tpch' 'vss')
