111d59721f75c86ea02b945d0b4c12e4acbf6e0d
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2025-03-10 10:57:43 +0100

    tested_binaries = [row[0] + "-" + row[1] + "_gcc4" if row[0] == 'linux' else row[0] + "-" + row[1] for row in result]

--- scripts/create_build_report.py
+++ scripts/create_build_report.py
@@ -109,7 +109,10 @@ def create_build_report(build_job, con):
                     """)
                 if failures_count > 0:
                     result = con.execute(f"SELECT nightly_build, duckdb_arch FROM '{ inputs }'").fetchall()
-                    tested_binaries = [row[0] + "-" + row[1] + "_gcc4" if row[0] == 'linux' else row[0] + "-" + row[1] for row in result]
+                    if branch == 'main':
+                        tested_binaries = [row[0] + "-" + row[1] + "_gcc4" if row[0] == 'linux' else row[0] + "-" + row[1] for row in result]
+                    else:
+                        tested_binaries = [row[0] + "-" + row[1] + "_gcc4" if row == 'linux_amd64' else row[0] + "-" + row[1] for row in result]
                 else:
                     result = con.execute(f"SELECT DISTINCT tested_platform FROM '{ ext_results }'").fetchall()
                     tested_binaries = [row[0] for row in result]
